5bab444008411f9ed83d758e82c58a305e8f6a2e
Benjamin VANDENBUSSCHE <13152@ecam.be>
2017-11-29 12:00:43 +0100

    Added Pin file

---
+++ Misc/JSONpins.py
@@ -0,0 +1,13 @@
+import json
+
+PinLU = 0
+PinLD = 1
+PinRU = 2
+PinRD = 3
+
+save_to_json = {'data': {'PinLU': PinLU, 'PinLD': PinLD, 'PinRU': PinRU, 'PinRD': PinRD}, "test": 'jdnfj'};
+
+with open('data.json', 'w') as outfile:
+	json.dump(save_to_json, outfile, indent=4)
+	
+outfile.close()
