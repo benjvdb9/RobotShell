d28379ca67d2aeb96055eb53169fdfb1f4b3f07c
Anizetho <thomas.anizet@hotmail.com>
2017-12-13 09:46:09 +0100

    Fix Bug

---
+++ gps_copie.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include <string.h>
+#include<fcntl.h>
+#include <stdbool.h>
+
+#define Taille 10000
+
+int main()
+{
+    int fd;
+    char buffer[Taille];
+    ssize_t result;
+    printf("test");
+}
+
