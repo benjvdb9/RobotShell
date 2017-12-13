9d7cb060843c22732ce05b62c311de68e55613ef
Amaury LEKENS <14027@ecam.be>
2017-12-13 10:38:01 +0100

    finish forward, backward, left, right

--- turnRight.c
+++ turnRight.c
@@ -4,8 +4,44 @@
 
 int main()
 {
-    char* pinToHigh[2] = {"11","12"};
-    char* pinToLow[2] = {"13","14"};
+    char* pinToHigh[2] = {};
+    char* pinToLow[2] = {};
+
+    FILE* file = NULL;
+    char line[2] = "";
+    file = fopen("PinData.txt", "r");
+
+    if (file != NULL)
+    {
+        int i = 0;
+        while (fgets(line, 2, file) != NULL) // On lit le fichier tant qu'on ne reçoit pas d'erreur (NULL)
+        {
+            switch (i)
+            {
+                case 0 :
+                    pinToHigh[0] = line;
+                    break;
+                case 1 :
+                    pinToLow[0] = line;
+                    break;
+                case 2 :
+                    pinToLow[1] = line;
+                    break;
+                case 3 :
+                    pinToHigh[1] = line;
+                    break;
+                default :
+                    break;
+            }
+
+            i++;
+        }
+
+        fclose(file);
+    }
+    {
+        printf("Impossible d'ouvrir le fichier test.txt");
+    }
     int i;
 
     for (i = 0 ; i < 2 ; i++)
@@ -18,6 +54,12 @@ int main()
     while(key!=115)
     {
         key = getchar();
+    }
+
+    for (i = 0 ; i < 2 ; i++)
+    {
+        setPin(pinToHigh[i],"0");
+        setPin(pinToLow[i],"0");
     }
 
     return 0;
