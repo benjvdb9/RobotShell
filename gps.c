4bf631a071448b4d04f043bf858509f2635d6791
Anizetho <thomas.anizet@hotmail.com>
2017-12-20 11:53:06 +0100

    GPS OK

--- gps.c
+++ gps.c
@@ -5,18 +5,18 @@
 #include<fcntl.h>
 #include<stdbool.h>
 
-#define Taille 10000
+#define SIZE 10000
 
 int main()
 {
     int fd;
-    char buffer[Taille];
+    char buffer[SIZE];
     ssize_t result;
     char *token;
-    int longueurChaine;
     char *SecondToken;
-    char *suiteChaine;
-    int LongueurSecondToken;
+    int LenToken;
+    int LenSecondToken;
+    char *ChoiceChar;
     int i;
 
     fd = open("/dev/ttyS0", O_RDONLY);
@@ -30,6 +30,7 @@ int main()
     while(1)
     {
         result = read(fd, buffer, sizeof(buffer));
+        
         // read fail
         if(result == -1)
         {
@@ -40,63 +41,64 @@ int main()
         // no data
         if(!result)
         {
-	    printf("no data\n");
-	    printf(buffer);
+            printf("no data\n");
+            printf(buffer);
         }
         
         // data received
         if(result > 0)
         {
             buffer[result] = 0;
-	    //printf(buffer);
+            
+            // Split data by $
+            token = strtok(buffer, "$");
 
-	    token = strtok(buffer, "$");
-            //printf("split '$' :  %s \n", token);
+            LenToken = strlen(token);
+            //printf("La chaîne : %s compte %d caractères. \n", token ,LenToken);
+            
+            // Specify the interresting data
+            ChoiceChar = strstr(token, "GPRMC");
+            if (ChoiceChar != NULL)
+                {
+                    /* Split "token data" by "," because with GPRMC, there are
+                     several data like speed, time, ... separated by ',' */
+                    SecondToken = strtok(ChoiceChar, ",");
+                    //printf("SecondToken : %s \n", SecondToken);
+                    
+                    i = 0;
+                    while(SecondToken != NULL)
+                    {
+                        SecondToken = strtok(NULL, ",");
+                        //printf("SecondToken : %s \n", SecondToken);
+                        //printf("Valeur de I : %d \n", i);
 
-	    longueurChaine = strlen(token);
-	    //printf("La chaîne : %s compte %d caractères. \n", token ,longueurChaine);
-    	    
-	    suiteChaine = strstr(token, "GPRMC");
-	    if (suiteChaine != NULL)
-    	    {
-	        //printf("Chaine voulue : %s \n", suiteChaine);
-		
-		SecondToken = strtok(suiteChaine, ",");
-    	    	//printf("SecondToken : %s \n", SecondToken);
-		
-		i = 0;
-		while(SecondToken != NULL)
-		{
-		    SecondToken = strtok(NULL, ",");
-		    //printf("SecondToken : %s \n", SecondToken);
-		    //printf("Valeur de I : %d \n", i);
+                        // To sort out the different SecondToken
+                        if (i<3)
+                        {
+                            LenSecondToken = strlen(SecondToken);
+                            //printf("LenSecondToken : %d \n", LenSecondToken);
+                            
+                            // To Longitude
+                            if (LenSecondToken == 10)
+                            {
+                                if (i==2) // 3rd elem to get (0 -> 1 -> 2)
+                                {
+                                    printf("Longitude : %s \n \n", SecondToken);
+                                }
+                                i++;
+                            }
 
-		    if (i<3)
-		    {
-			LongueurSecondToken = strlen(SecondToken);
-			//printf("LongueurSecondToken : %d \n", LongueurSecondToken);
-			
-			// Pour la Longitude
-		        if (LongueurSecondToken == 10)
-		        {
-			    if (i==2) // car 3ème élément à prendre (0 -> 1 -> 2)
-			    {
-				printf("Longitude : %s \n \n", SecondToken);
-			    }
-			    i++;
-		        }
-
-			// Pour la latitude
-			if (LongueurSecondToken == 9)
-			{
-			    printf("Latitude : %s \n", SecondToken);
-			    i++;
-			}
-		    }
-		}
-	      }
+                            // To latitude
+                            if (LenSecondToken == 9)
+                            {
+                                printf("Latitude : %s \n", SecondToken);
+                                i++;
+                            }
+                        }
+                     }
+                 }
+             }
         }
-    }
     close(fd);
     return (EXIT_SUCCESS);
 }
